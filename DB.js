d876759d81562279ac87d5a4cbf3b06a70e01e9d
Witapoke <ignaciomanganaro22@gmail.com>
2025-03-18 11:01:16 -0300

    sipipepe

--- DB.js
+++ DB.js
@@ -3,12 +3,10 @@ const PORT = process.env.PORT
 const connectionString = process.env.MONGO_DB_URL
 import app from './index.js'
 
-async function connectDB() {
-  const connection = await mongoose.connect(connectionString)
-  app.listen(PORT, () => {
-    console.log('App listening to PORT 5000')
-  })
-  return connection
+export default async function connectDB() {
+  await mongoose
+    .connect(connectionString)
+    .then(() =>
+      app.listen(PORT, () => console.log('App listening to port 5000'))
+    )
 }
-
-export default connectDB
